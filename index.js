f18ac52ceaf29ed33b1772ad36cdbcd70c3c8a2e
Richie Lollis <richiedlollis@gmail.com>
2022-06-07 21:28:13 -0400

    Question layout

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('hello world');
+const inquirer = require('inquirer');
+
+//a way to ask user which currencies to convert 
+
+const questions = [
+    {
+        type: 'input',
+        name: 'baseCurrency',
+        message: 'Base Currency',
+    },
+    {
+        type: 'number',
+        name: 'amount',
+        message: 'Amount',
+        validate(value) {
+            const pass = !isNaN(value);
+            if (pass) {
+                return true;
+            } 
+
+            return 'Please enter valid number';
+        }
+    },      
+    {
+        type: 'input',
+        name: 'targetCurrency',
+        message: 'Target Currency',
+    },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(answers); 
+});
+
+//a way to do the conversion 
+
+const conversion = (base, target) => {
+
+}
+
+
