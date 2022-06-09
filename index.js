9451c2c37616407f4e0b306074d560977886962f
Richie Lollis <richiedlollis@gmail.com>
2022-06-08 22:23:50 -0400

    Conversion funciton working.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const axios = require('axios');
 
 //a way to ask user which currencies to convert 
 
@@ -28,14 +29,30 @@ const questions = [
     },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-    console.log(answers); 
-});
-
 //a way to do the conversion 
 
-const conversion = (base, target) => {
-
+const conversion = (base, target, amount) => {
+    const headers = {
+        apikey: 'aFaADVdrgqEEMwlcRukJ8DGQLwTAEa9G'
+    }
+    axios.get(`https://api.apilayer.com/fixer/convert?to=${target}&from=${base}&amount=${amount}`, {headers}).then(({data}) => {            
+        //Amount Base is equal to Result Target at a rate of Rate 
+        console.log(`${amount} ${base} = ${data.result} ${target} at a rate of ${data.info.rate}`);
+    })
+    .catch((error) => {
+        //find error message
+        //present to user
+        //ask user to go again
+        //inquirer docs a good place to start
+        //create a goAgain function after .then
+        console.log(error);
+    })
 }
 
+const askQuestions = () => {inquirer.prompt(questions).then((answers) => {
+        //console.log(answers); 
+        conversion(answers.baseCurrency, answers.targetCurrency, answers.amount);
+    });
+}
 
+askQuestions();
